0638bb06ad2978703e2769501e6ca16d929f786d
feg59crz <fernandogomescruz@gmail.com>
2023-07-28 10:58:12 -0300

    host update

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from uti import app
 import os
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
