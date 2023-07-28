e7106a840cc8fa04250c051041d920e819da0f7b
feg59crz <fernandogomescruz@gmail.com>
2023-07-28 10:51:09 -0300

    update on port

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from uti import app
+import os
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
